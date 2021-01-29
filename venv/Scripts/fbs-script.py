26e0c59b30da59ecad7a9d3eed4fc97ee057f920
Pierre Verhulst <pierreverhulst97@live.com>
2021-01-29 17:06:35 +0100

    Venv Files for the IWAST Configurator V2

--- venv/Scripts/fbs-script.py
+++ venv/Scripts/fbs-script.py
@@ -1,12 +1,33 @@
-#!c:\users\calle\documents\github\qt-config\venv\scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'fbs==0.8.4','console_scripts','fbs'
-__requires__ = 'fbs==0.8.4'
+#!C:\Users\Pierre\PycharmProjects\QT_Config_V2\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'fbs==0.9.0','console_scripts','fbs'
 import re
 import sys
-from pkg_resources import load_entry_point
+
+# for compatibility with easy_install; see #2198
+__requires__ = 'fbs==0.9.0'
+
+try:
+    from importlib.metadata import distribution
+except ImportError:
+    try:
+        from importlib_metadata import distribution
+    except ImportError:
+        from pkg_resources import load_entry_point
+
+
+def importlib_load_entry_point(spec, group, name):
+    dist_name, _, _ = spec.partition('==')
+    matches = (
+        entry_point
+        for entry_point in distribution(dist_name).entry_points
+        if entry_point.group == group and entry_point.name == name
+    )
+    return next(matches).load()
+
+
+globals().setdefault('load_entry_point', importlib_load_entry_point)
+
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
-    sys.exit(
-        load_entry_point('fbs==0.8.4', 'console_scripts', 'fbs')()
-    )
+    sys.exit(load_entry_point('fbs==0.9.0', 'console_scripts', 'fbs')())
